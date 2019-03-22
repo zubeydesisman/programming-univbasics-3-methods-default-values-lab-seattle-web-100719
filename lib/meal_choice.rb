902b3ce367035c9ca1ac958e720529e93ddc8537
Kellye Greene <kellyecodes@gmail.com>
2019-03-22 11:36:46 -0400

    Updates README and solution

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,9 +1,3 @@
-# Define a method `meal_choice`
-# It takes 2 veggies and protein defaults to meat
-# It returns `meal` with a value of
-# "A plate of #{protein} with #{veg1} and #{veg2}"
-# and `puts` "What a nutritious meal!"
-
 def meal_choice(veg1, veg2, protein='meat')
   puts "What a nutritious meal!"
   meal = "A plate of #{protein} with #{veg1} and #{veg2}."
